144550f2dafa663ab7a1d98e77579da4e773d378
dldouglaslopes <dldouglaslopes@gmail.com>
2018-05-09 17:13:18 -0300

    Create repository and service to Category

--- src/main/java/com/douglas/cursomc/resources/CategoryResource.java
+++ src/main/java/com/douglas/cursomc/resources/CategoryResource.java
@@ -1,27 +1,26 @@
 package com.douglas.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglas.cursomc.domain.Category;
+import com.douglas.cursomc.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Category> list() {
-		Category categoryA = new Category(1, "Informatics");	
-		Category categoryB = new Category(2, "Office");
+	@Autowired
+	private CategoryService service;
+	
+	@RequestMapping(value= "/{id}" , method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Category category = service.search(id);
 		
-		List<Category> categories = new ArrayList<>();
-		categories.add(categoryA);
-		categories.add(categoryB);		
-		
-		return categories;
+		return ResponseEntity.ok().body(category);
 	}
 }
