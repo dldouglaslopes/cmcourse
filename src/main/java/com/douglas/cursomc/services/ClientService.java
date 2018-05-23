b018edf3b520cb4c5f642e36e3d07ecd924073c5
dldouglaslopes <dldouglaslopes@gmail.com>
2018-05-23 16:03:30 -0300

    Adjust the deletion of the client

--- src/main/java/com/douglas/cursomc/services/ClientService.java
+++ src/main/java/com/douglas/cursomc/services/ClientService.java
@@ -59,7 +59,7 @@ public class ClientService {
 		try {
 			repository.deleteById(id);			
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("It is not possible to delete because there are related entities.");
+			throw new DataIntegrityException("It is not possible to delete because there are related orders.");
 		}
 	}
 
