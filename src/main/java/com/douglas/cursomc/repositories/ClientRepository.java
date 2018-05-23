9796aacb70d09f5d9f2b9b25841f22678b8e28d1
dldouglaslopes <dldouglaslopes@gmail.com>
2018-05-23 19:50:53 -0300

    Custom Validation: email can't be repeated in the insertion of the client

--- src/main/java/com/douglas/cursomc/repositories/ClientRepository.java
+++ src/main/java/com/douglas/cursomc/repositories/ClientRepository.java
@@ -1,12 +1,14 @@
 package com.douglas.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.douglas.cursomc.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository< Client, Integer>{
 
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
 }
--- src/main/java/com/douglas/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/douglas/cursomc/services/validation/ClientInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.douglas.cursomc.domain.Client;
 import com.douglas.cursomc.domain.enums.TypeClient;
 import com.douglas.cursomc.dto.ClientNewDTO;
+import com.douglas.cursomc.repositories.ClientRepository;
 import com.douglas.cursomc.resources.exceptions.FieldMessage;
 import com.douglas.cursomc.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO>{
 
+	@Autowired
+	private ClientRepository clientRepository ;
+	
 	@Override
 	public void initialize(ClientInsert clientInsert) {
 	}
@@ -27,6 +34,12 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		
 		if (value.getType().equals(TypeClient.LEGALENTITY.getCode()) && !BR.isValidCNPJ(value.getCpfOrCnpj())) {
 			fieldMessages.add(new FieldMessage("type", "Invalid CNPJ"));
+		}
+		
+		Client client = clientRepository.findByEmail(value.getEmail());
+		
+		if (client != null) {
+			fieldMessages.add(new FieldMessage("type", "Existing email."));
 		}
 		
 		for (FieldMessage fieldMessage : fieldMessages) {
